eca9b68dc97e86bb13f4097174df05458a550ec2
Fahad-Alajmi <alajmi771@gmail.com>
2019-05-10 08:03:09 +1200

    Added cart and details screen

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,19 @@ import styled from 'styled-components'
 
 export const ButtonContainer = styled.button`
 text-transform: capitalized;
-font-size:1.4rem;
+font-size:1rem;
 background: transparent;
-border: 0.05rem solid var(--mainDark);
-color: var(--mainDark);
-border-radius: 0.5rem;
+border: 0.05rem solid black;
+background-color: ${props => props.cartBtnInProductDetails ? "green" : "transparent"};
+color: black;
+border-radius: 2rem;
 padding: 0.2rem 0.5rem;
 cursor: pointer;
 margin: 0.2rem 0.5rem 0.2rem 0;
 transition: all 0.5s ease-in-out;
 &:hover{
-    background:var(--mainDark);
-    color:var(--mainWhite);
+    background:black;
+    color:white;
 }
 &:focus {
     outline: none;
