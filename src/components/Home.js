1cd31bb7693ff0c62cc24fede9e65b3997eb531a
ReubenWhite <reubwhite@hotmail.com>
2019-05-24 01:09:49 +1200

    background/specials images/ added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,8 @@ import HomeNews from './HomeNews';
 import './Home.css';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from './img/Forest1.jpg';
-import image2 from './img/Apple-tree.png';
-import image3 from './img/Forest1.jpg';
+import image1 from './img/oak-tree1.png';
+import image2 from './img/Willow-tree.png';
 
 export default class Home extends Component {
 
@@ -20,13 +19,15 @@ export default class Home extends Component {
           <React.Fragment>
             {/* <header className="App-header"></header> */}
 
+          
             <div className="py-5">
+            <h2 className='title'>Today's Specials<br/></h2>
               <div className="container">
                 <div>
                   <AliceCarousel className="carousel" stagePadding='10' fadeOutAnimation buttonsDisabled dotsDisabled autoPlay autoPlayInterval='3000' >
                     <img src={image1} onDragStart={console.log('Hi Reub')} className="specials2" />
                     <img src={image2} onDragStart={console.log('Hi Reub')} className="specials1" />
-                    <img src={image3} onDragStart={console.log('Hi Reub')} className="specials1" />
+
                   </AliceCarousel>
                 </div>
                 
--- src/components/HomeNews.js
+++ src/components/HomeNews.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
-class HomeNews extends Component{
+class HomeNews extends Component {
 
-	constructor(){
+	constructor() {
 		super();
 		this.state = {
 			specials: [],
@@ -12,23 +12,22 @@ class HomeNews extends Component{
 
 	render() {
 		return (
-			
+
 			<div>
-				
 				<h2>News</h2>
 				<p1>Resilience of Yellowstone's forests tested by unprecedented fire</p1>
-
-				<p> Researchers describe what happens when Yellowstone -- 
-					adapted to recurring fires every 100 to 300 years -- 
-					instead burns twice in fewer than 30 years. Yellowstone as we know it faces an uncertain future, 
-					the researchers say, and one of the big questions they hope to answer is whether the forests can recover.
+				<br /><br />
+				<p> Researchers describe what happens when Yellowstone adapted<br />
+					to recurring fires every 100 to 300 years
+					instead burns twice in fewer<br />
+					than 30 years.Yellowstone as we know it faces an uncertain future,<br />
+					the researchers say, and one of the big questions
+					they hope to answer<br />
+					is whether the forests can recover.
 				</p>
-				
-                
 			</div>
-        );
-    }
+		);
+	}
 }
-
 
 export default HomeNews;
