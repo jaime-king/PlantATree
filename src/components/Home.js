f17da643c8152b24c046a64e6db0eaf70606a567
JimDeDev <jaimeking00@gmail.com>
2019-05-23 20:34:06 +1200

    Carousel working

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,37 +2,34 @@ import React, { Component } from 'react';
 import HomeSpecials from './HomeSpecials';
 import HomeNews from './HomeNews';
 import './Home.css';
-import BackgroundSlideshow from 'react-background-slideshow';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from './img/Forest1.png';
-import image2 from './img/Forest1.png';
-import image3 from './img/Forest1.png';
+import image1 from './img/Forest1.jpg';
+import image2 from './img/Apple-tree.png';
+import image3 from './img/Forest1.jpg';
 
 export default class Home extends Component {
 
   constructor(props) 
   {
-    this.handleOnDragStart = e => e.preventDefault();
+    super();
   }
+
       render() {
         return (
           <React.Fragment>
-            <div>
-              <BackgroundSlideshow images={[image1, image2, image3]} />
-            </div>
-            <div>
-              <AliceCarousel mouseDragEnabled >
-                <img src="/Apple-tree.png" onDragStart={handleOnDragStart} className="specials1" />
-                <img src="/Mahogany.png" onDragStart={handleOnDragStart} className="specials2" />
-              </AliceCarousel>
-            </div>
-
-            <header className="App-header"></header>
+            {/* <header className="App-header"></header> */}
 
             <div className="py-5">
               <div className="container">
-                <h2 className="text-center"> Home Page</h2>
+                <div>
+                  <AliceCarousel className="carousel" stagePadding='10' fadeOutAnimation buttonsDisabled dotsDisabled autoPlay autoPlayInterval='3000' >
+                    <img src={image1} onDragStart={console.log('Hi Reub')} className="specials2" />
+                    <img src={image2} onDragStart={console.log('Hi Reub')} className="specials1" />
+                    <img src={image3} onDragStart={console.log('Hi Reub')} className="specials1" />
+                  </AliceCarousel>
+                </div>
+                
                 <HomeSpecials></HomeSpecials>
                 <HomeNews></HomeNews>
               </div>
@@ -40,4 +37,5 @@ export default class Home extends Component {
           </React.Fragment>
         );
       }
+
 }
